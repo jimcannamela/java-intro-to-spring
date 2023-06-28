bba06c0190b82243e4c8220c66bf9cba443f05d6
Jim Cannamela <jim.cannamela@outlook.com>
2023-06-28 16:14:42 -0500

    Second Test Case Working

--- src/test/java/com/example/demo/HelloControllerTests.java
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-// First Use Case
-// When pointing a browser at the URI, /hello, the endpoint should return the string "Hello World"
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,10 +11,21 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 public class HelloControllerTests {
 	@Autowired
 	MockMvc mockMvc;
+
+	// First Use Case
+	// When pointing a browser at the URI, /hello, the endpoint should return the string "Hello World"
 	@Test
 	void sayHello_noArgs_returnsHelloWorld() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string("Hello World"));
+				.andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+	}
+
+	// Second Use Case
+	@Test
+	void sayHello_withArgs_returnsHelloName() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello?name=Jim"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello, Jim!"));
 	}
 }
